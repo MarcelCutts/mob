4bf64c838b87be7aa4f07252446bd9603ea6cb0f
Marcel Cutts <marcel.cutts@red-badger.com>
2017-04-04 23:33:40 +0100

    Initial commit

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.header}>Mob!</Text>
+        <Text>The mob programming application</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  header: {
+    fontSize: 24,
+  }
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# Mob!
+
+A simple application to manage the humans that make up in mob in [mob programming](https://en.wikipedia.org/wiki/Mob_programming).
